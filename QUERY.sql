8fd4b23d7b0bbab2f03d5a5b0b5b20d79d54f00c
shantoopaul <shantoopaul@gmail.com>
2026-06-14 16:23:19 +0600

    add: query-6

--- QUERY.sql
+++ QUERY.sql
@@ -133,10 +133,10 @@ SELECT
   u.full_name,
   m.fixture,
   ROUND(b.total_cost) AS total_cost
-FROM Bookings b
-INNER JOIN Users u
+FROM Bookings AS b
+INNER JOIN Users AS u
   ON b.user_id = u.user_id
-INNER JOIN Matches m
+INNER JOIN Matches AS m
   ON b.match_id = m.match_id;
 
 -- =============================================================================================================================================
@@ -149,3 +149,16 @@ SELECT
 FROM Users AS u
 LEFT JOIN Bookings AS b
   ON u.user_id = b.user_id;
+
+-- =======================================================================================================================
+-- Query 6: Find all ticket bookings where the total cost is strictly higher than the average cost of all ticket bookings.
+-- =======================================================================================================================
+SELECT
+  booking_id,
+  match_id,
+  ROUND(total_cost) AS total_cost
+FROM Bookings
+WHERE total_cost > (
+    SELECT AVG(total_cost)
+    FROM Bookings
+);
